d1c97f37e12bf3324b0fae00692844f00a8be0c0
Rommel <68463203+rommelsim@users.noreply.github.com>
2025-02-19 10:55:06 +0800

    Update video.bat

--- Robot/video.bat
+++ Robot/video.bat
@@ -1,9 +1,12 @@
 @echo off
 
+REM Get the current script directory
+set SCRIPT_DIR=%~dp0
+
 REM Path to the executable
 set MODE=2
 set numVideos=1
-set EXECUTABLE_PATH=VideoAPI.exe
+set EXECUTABLE_PATH="%SCRIPT_DIR%VideoAPI.exe"
 
 REM Run the executable with the user-provided values
 "%EXECUTABLE_PATH%" %numVideos% %MODE%
