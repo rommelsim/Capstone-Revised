5705ebfd55f2626e955a711d6b16b01812a6b26b
Rommel <68463203+rommelsim@users.noreply.github.com>
2025-03-25 16:41:04 +0800

    Update

--- Robot/BlackWidow.py
+++ Robot/BlackWidow.py
@@ -7,7 +7,8 @@ isFinished = False
 def PerformChromaTestBlackWidowV4():
     global isFinished
 
-    lighting_selector = '#root > div > div.nav-tabs > div.navs-wrapper > div:nth-child(2)'
+    # lighting_selector = '#root > div > div.nav-tabs > div.navs-wrapper > div:nth-child(2)'
+    lighting_selector = '#root > div > div.nav-tabs > div.navs-wrapper > div.nav.active'
     tmp = '#body-wrapper > div > div.widget-col.col-right.flex > div > div > div:nth-child(2) > div.modes-area.active > div.flex.chroma-flex-row > div.dropdown-area > div.s3-dropdown > div.selected.raw-text'
     static = '#body-wrapper > div > div.widget-col.col-right.flex > div > div > div:nth-child(2) > div.modes-area.active > div.flex.chroma-flex-row > div.dropdown-area > div.s3-options.unsetZ.flex.expand > div:nth-child(9)'
 
